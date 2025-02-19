720b94592902ba07addb400c43fc1810d3e92cd1
Wonbin Lee <wblee800@gmail.com>
2025-02-19 06:50:45 +0000

    Edit docusaurus config

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://wblee.site',
+  url: 'https://wblee800.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'wblee800', // Usually your GitHub org/user name.
+  projectName: 'wblee800.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
